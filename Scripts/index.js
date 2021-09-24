160880067e9a28d162bad0e1488f968c930626c6
Thomas Baloyi <baloyi.thomas@outlook.com>
2021-09-24 13:14:59 +0200

    Update

---
+++ Scripts/index.js
@@ -0,0 +1,8 @@
+/* https://reciosonny.com/how-to-integrate-reactjs-in-existing-asp-net-mvc-app/ */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+ 
+const App = () => <div>Hello world!</div>;
+ 
+ReactDOM.render(<App />, document.getElementById("root"));
