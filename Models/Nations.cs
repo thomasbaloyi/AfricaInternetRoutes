bd56d3f7f045e19f6e8efbace286c0e48db2c2a4
DeKezster7 <mabikatc@gmail.com>
2021-09-24 10:44:50 +0200

    Add functionality to add alternative country names

--- Models/Nations.cs
+++ Models/Nations.cs
@@ -2,10 +2,22 @@ using System;
 
 
 namespace AfricaInternetRoutes.Models {
+	/// <summary>
+    /// Class
+    /// <c>Nations</c>
+    /// represents an African and its recognizable titles
+    /// </summary>
 	public class Nations {
 		public string officialName { get; set; }  // The official name of the country
 		private var alternativeNames = new ArrayList();
 
+		/// <summary>
+        /// method
+        /// <c>checkName</c>
+        /// assesses a given name to see whether it is an alternative name for this country.
+        /// </summary>
+        /// <param name="nameToCheck"></param>
+        /// <returns>True or false result of assessment of possible alternative name</returns>
 		public bool checkName(string nameToCheck) {
 			bool answer = false;  // value of whether name is an alternative title for a country.
 			// check each alternative name for this country and compare with name to check.
@@ -20,6 +32,27 @@ namespace AfricaInternetRoutes.Models {
 			return answer; 
 		}
 
-		//public void addAlternative
+        /// <summary>
+        /// method
+        /// <c>addAlternative</c>
+        /// appends alternative country name to the list of country titles.
+        /// </summary>
+        /// <param name="alternativeTitle"></param>
+		public void addAlternative(string alternativeTitle)
+        {
+			alternativeNames.Add(alternativeTitle);  // add name to list of alternative names.
+        }
+
+        /// <summary>
+        /// method
+        /// <c>addAlternative</c>
+        /// appends list of alternative country names to the list of country titles.
+        /// </summary>
+        /// </summary>
+        /// <param name="arrayList"></param>
+        public void addAlternative(ArrayList arrayList)
+        {
+            alternativeNames.AddRange(arrayList);  // append arraylist to list of alternative names.
+        }
 	}
 }
