42bd3f86d2f2ca33a6b22ae97d24f369a1b0e603
DeKezster7 <mabikatc@gmail.com>
2021-10-02 16:00:01 +0200

    Added functionality to AfricaApp.js

---
+++ FrontEnd/src/AfricaApp.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import L from 'leaflet'; //import leaflet so as to utilise it for mapping datapoints onto the map of Africa.
+import { Map, TileLayer, GeoJSON } from 'react-leaflet';
+import './AfricaApp.css';  // import the css
+import 'leaflet/dist/leaflet.css'; //import leaflet css so as to utilise it for mapping datapoints onto the map of Africa.
+import exampleJson from './locationsJson/example.json'; //import the json file with the details of points that need to be mapped onto the map of Africa.
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+//apply blue pins as markers for the mapping.
+L.Icon.Default.mergeOptions({
+   iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+   iconUrl: require('leaflet/dist/images/marker-icon.png'),
+   shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+});
+
+const initialAfricaZoom = 2; //set the initial zoom in so that the user sees the whole continent of Africa only.
+const coordinatesAfricaCenter = [7.943625, 24.329730]; //set center point so that continent appears centrally in the display.
+
+function AfricaApp() {
+    return (
+        <div className="AfricaApp">
+            <Map center={defaultCenter} zoom={defaultZoom}>
+                <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" attribution="&copy; <a href=&quot;https://www.openstreetmap.org/copyright&quot;>OpenStreetMap</a> contributors" />
+                <GeoJSON data={exampleJson} />
+            </Map>
+        </div>
+    );
+}
+
+export default AfricaApp;
