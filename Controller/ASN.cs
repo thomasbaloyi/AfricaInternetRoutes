c138e528cbef947fa0db4da9e7beb3e4356a9606
Thomas Baloyi <baloyi.thomas@outlook.com>
2021-10-06 04:44:50 +0200

    Remove ASN.cs constructor and change instance variable protection to public

--- Controller/ASN.cs
+++ Controller/ASN.cs
@@ -2,24 +2,12 @@
 
 public class ASN
 {
-	private int asn_num { get; set; }
-	private String reg_date { get; set; }
-	private String country { get; set; }
-	private String region { get; set; }
-	private int reg_year { get; set; }
-	private String industry { get; set; }
-	private String org_category { get; set; }
-	private String asn_type { get; set; }
-
-	public ASN(int asn_num, String reg_date, String country, String region, int reg_year, String industry, String org_category, String asn_type)
-	{
-		this.asn_num = asn_num;
-		this.reg_date = reg_date;
-		this.country = country;
-		this.region = region;
-		this.reg_year = reg_year;
-		this.industry = industry;
-		this.org_category = org_category;
-		this.asn_type = asn_type;
-	}
+	public int asn_num { get; set; }
+	public String reg_date { get; set; }
+	public String country { get; set; }
+	public String region { get; set; }
+	public int reg_year { get; set; }
+	public String industry { get; set; }
+	public String org_category { get; set; }
+	public String asn_type { get; set; }
 }
