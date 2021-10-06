9fb81727106424ba8b8df1dca29d93314707fd32
Thomas Baloyi <baloyi.thomas@outlook.com>
2021-10-06 15:22:19 +0200

    Upload ConeCustomer.cs to represent a cone autonomous system (ASN) and the customer ASNs it is able to reach

---
+++ Controller/ConeCustoner.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+
+public class ConeCustomer
+{
+    public int cone_as { get; set; }
+    public List<Customer> customers { get; set; }
+}
+
+public class Customer {
+    public int customer_as { get; set; }
+}
