e8e73359a692093d4e0e6b0bc01ce95605a2175a
Thomas Baloyi <baloyi.thomas@outlook.com>
2021-10-06 03:41:12 +0200

    Update DataController.cs to download and process data from stats.afrinic.net

--- Controller/DataController.cs
+++ Controller/DataController.cs
@@ -16,11 +16,11 @@ public class DataController
 	/** Stores all unique ASNs */
 	private static Dictionary<int, ASN> dictionary;
 
-	static string AFRINIC_LINK = "https://stats.afrinic.net/index.php/download/asndata.csv" ;
+	static string AFRINIC_URI = "https://stats.afrinic.net/index.php/download/asndata.csv" ;
 
-	static string CAIDA_LINK = "https://publicdata.caida.org/datasets/as-relationships/serial-2/20210901.as-rel2.txt.bz2";
+	static string CAIDA_URI = "https://publicdata.caida.org/datasets/as-relationships/serial-2/20210901.as-rel2.txt.bz2";
 	
-	static string pch = "https://www.pch.net/ixp/data" ;
+	static string PCH_URI = "https://www.pch.net/ixp/data" ;
 
 	static string ripe = "https://www.maxmind.com/en/geoip2-services-and-databases" ;
 
@@ -29,16 +29,18 @@ public class DataController
 	public static void Main(String[] args)
 	{
 
-		downloadData(AFRINIC_LINK, "asndata.csv");
-		// process data
-		//DataController.processAfrinic();
+		downloadData(AFRINIC_URI, "asndata.csv");
+		downloadData(CAIDA_URI, "20210901.as-rel2.txt.bz2");
+		processAfrinicData();
+
+
 		Console.Write("Done");
 	}
 	
 	/// <summary>
     /// Adds data from asndata.csv to a dictionary
     /// </summary>
-	private static void processAfrinic() 
+	private static void processAfrinicData() 
 	{
 		StreamReader afrinic = DataController.readData("asndata.csv");
 		dictionary = new Dictionary < int, ASN > (0);
@@ -46,10 +48,9 @@ public class DataController
 		while (!afrinic.EndOfStream)
 		{
 			String[] asn = afrinic.ReadLine().Split(",");
-			Console.WriteLine(asn[0]);
 			if (asn.Length == 8)
 			{
-				dictionary[int.Parse(asn[0])] = new ASN(int.Parse(asn[0]), asn[1], asn[2], asn[3], int.Parse(asn[4]), asn[5], asn[6], asn[7]);
+				dictionary.Add(int.Parse(asn[0]), new ASN(int.Parse(asn[0]), asn[1], asn[2], asn[3], int.Parse(asn[4]), asn[5], asn[6], asn[7]));
 			}
 		}
 		afrinic.Close();
@@ -64,33 +65,16 @@ public class DataController
 	{
 		WebClient webClient = new WebClient();
 		webClient.DownloadFile(link, "../RawData/" + filename);
-		
-		
-		/*try
-		{
-			var inputStream = new java.io.BufferedInputStream(link.openStream());
-			var outputStream = new FileStream("../RawData/" + filename);
-			byte[] data = new byte[1024];
-			int byteContent;
-			while ((byteContent = inputStream.read(data, 0, 1024)) != -1)
-			{
-				outputStream.Write(data, 0, byteContent);
-			}
-		}
-		catch (java.io.IOException e)
-		{
-			Console.WriteLine(e.toString());
-		} */
 	}
-	/**
-		* Given filename, reads data from a file the dataset directory into a scanner
-		*
-		* @return scanner containing data with data from file.
-		* @throws FileNotFoundException
-		*/
+	
+	/// <summary>
+    ///  Reads file data to a StreamReader
+    /// </summary>
+    /// <param name="filename"></param>
+    /// <returns></returns>
 	private static StreamReader readData(String filename) 
 	{
-		StreamReader reader = new StreamReader(filename);
+		StreamReader reader = new StreamReader("../RawData/" + filename);
 		return reader;
 	}
 }
