e10e3ba3c5f72f2fb0ac13267bc7706dbd686f17
Thomas Baloyi <baloyi.thomas@outlook.com>
2021-10-06 11:30:05 +0200

    Update

---
+++ Controller/PeerPeerASRelationship.cs
@@ -0,0 +1,6 @@
+public class PeerPeerASRelationship
+{
+    public int peer_as1 { get; set; }
+    public int peer_as2 { get; set; }
+    public string source { get; set; }
+}
---
+++ Controller/ProviderCustomerASRelationship.cs
@@ -0,0 +1,6 @@
+public class ProviderCustomerASRelationship
+{
+    public int provider_as { get; set; }
+    public int customer_as { get; set; }
+    public string source { get; set; }
+}
