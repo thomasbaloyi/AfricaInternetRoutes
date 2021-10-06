84bc5c7ced0757e6156a71c11def91d0ad593a59
DeKezster7 <mabikatc@gmail.com>
2021-10-06 22:03:39 +0200

    ASNs now mapping in codesandbox.io

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,36 +1,34 @@
-import React from 'react';
-import L from 'leaflet'; //import leaflet so as to utilise it for mapping datapoints onto the map of Africa.
-import { Map, TileLayer, GeoJSON } from 'react-leaflet';
-import './App.css';  // import the css
-import 'leaflet/dist/leaflet.css'; //import leaflet css so as to utilise it for mapping datapoints onto the map of Africa.
-import { json } from 'd3-fetch';
-import { node } from 'prop-types';
+import React from "react";
+import L from "leaflet"; //import leaflet so as to utilise it for mapping datapoints onto the map of Africa.
+import { Map, TileLayer, GeoJSON } from "react-leaflet";
+import "./App.css"; // import the css
+import "leaflet/dist/leaflet.css"; //import leaflet css so as to utilise it for mapping datapoints onto the map of Africa.
 //import exampleJson from './locationsJson/example.json'; //import the json file with the details of points that need to be mapped onto the map of Africa.
-import ASNData from '../ProcessedData/ASNData.json'
-import CapitalLocations from './locationsJson/CountryNames.json'
+import ASNData from "../ProcessedData/ASNData.json";
+import CapitalLocations from "./locationsJson/CountryNames.json";
 
 //initial geojson which will be used to map points onto the map.
 const geoJsonInit = {
-    "type": "FeatureCollection",
-    "features": []
+    type: "FeatureCollection",
+    features: []
 };
 
 //function to create geoJson data
-function populateAsnData(abbreviation,nationName,capitalLocation) {
+function populateAsnData(abbreviation, nationName, capitalLocation) {
     var jsonObj = {
-        "type": "Feature",
-        "id": abbreviation,  // assign the country's abbreviation as its ID.
-        "properties": {
-            "name": nationName,  //add country Name.
-            "popupContent": nationName
+        type: "Feature",
+        id: abbreviation, // assign the country's abbreviation as its ID.
+        properties: {
+            name: nationName, //add country Name.
+            popupContent: nationName
         },
-        "geometry": {
-            "type": "Point", // indicate that this a point on the map.
-            "coordinates": capitalLocation  // add in the country's latitude and longitude using the capital city.
+        geometry: {
+            type: "Point", // indicate that this a point on the map.
+            coordinates: capitalLocation // add in the country's latitude and longitude using the capital city.
         }
     };
 
-    return jsonObj;  // return the json object to be pushed to the features array. 
+    return jsonObj; // return the json object to be pushed to the features array.
 }
 
 /**
@@ -44,74 +42,144 @@ function geoJsonASNs() {
      * The coordinates of each nation will be retrieved from CountryNames.json
      */
 
-    var asnLocations = geoJsonInit;  // add in the initial elements.
-    
+    var asnLocations = geoJsonInit; // add in the initial elements.
 
     //loop through asnData
     ASNData.map((asnfileData) => {
         var countryName = asnfileData.country;
         //loop through capitalLocations to compare with countryName
         CapitalLocations.map((locationData) => {
-            /* 
+            /*
              * compare countryName with all possible names for a country as listed in CountryNames.
              * if it matches then use populateAsnData function to create the data for our geojson.
              * push this data into the functions array in asnLocations.
              * if we do not find a match then we will assume the name used in the datafile is not a recognised one or we were unable to obtain the country's location.
              */
-            if ((countryName.toLowerCase() === locationData.name.toLowerCase()) || (countryName.toLowerCase() === ("\u0022" + locationData.name.toLowerCase() + "\u0022"))) {
-                asnLocations.features.push(populateAsnData(locationData.abbreviation, locationData.name, locationData.coordinates));
-                break;
-            }
-            else if ((countryName.toLowerCase() === locationData.alternativeName1.toLowerCase()) || (countryName.toLowerCase() === ("\u0022" + locationData.alternativeName1.toLowerCase() + "\u0022"))) {
-                asnLocations.features.push(populateAsnData(locationData.abbreviation, locationData.name, locationData.coordinates));
-                break;
-            }
-            else if ((countryName.toLowerCase() === locationData.alternativeName2.toLowerCase()) || (countryName.toLowerCase() === ("\u0022" + locationData.alternativeName2.toLowerCase() + "\u0022"))) {
-                asnLocations.features.push(populateAsnData(locationData.abbreviation, locationData.name, locationData.coordinates));
-                break;
-            }
-            else if ((countryName.toLowerCase() === locationData.alternativeName3.toLowerCase()) || (countryName.toLowerCase() === ("\u0022" + locationData.alternativeName3.toLowerCase() + "\u0022"))) {
-                asnLocations.features.push(populateAsnData(locationData.abbreviation, locationData.name, locationData.coordinates));
-                break;
-            }
-            else if ((countryName.toLowerCase() === locationData.alternativeName4.toLowerCase()) || (countryName.toLowerCase() === ("\u0022" + locationData.alternativeName4.toLowerCase() + "\u0022"))) {
-                asnLocations.features.push(populateAsnData(locationData.abbreviation, locationData.name, locationData.coordinates));
-                break;
-            }
-            else if ((countryName.toLowerCase() === locationData.alternativeName5.toLowerCase()) || (countryName.toLowerCase() === ("\u0022" + locationData.alternativeName5.toLowerCase() + "\u0022"))) {
-                asnLocations.features.push(populateAsnData(locationData.abbreviation, locationData.name, locationData.coordinates));
-                break;
-            }
-            else if ((countryName.toLowerCase() === locationData.alternativeName6.toLowerCase()) || (countryName.toLowerCase() === ("\u0022" + locationData.alternativeName6 + "\u0022"))) {
-                asnLocations.features.push(populateAsnData(locationData.abbreviation, locationData.name, locationData.coordinates));
-                break;
+            if (
+                countryName.toLowerCase() === locationData.name.toLowerCase() ||
+                countryName.toLowerCase() ===
+                "\u0022" + locationData.name.toLowerCase() + "\u0022"
+            ) {
+                asnLocations.features.push(
+                    populateAsnData(
+                        locationData.abbreviation,
+                        locationData.name,
+                        locationData.coordinates
+                    )
+                );
+            } else if (
+                countryName.toLowerCase() ===
+                locationData.alternativeName1.toLowerCase() ||
+                countryName.toLowerCase() ===
+                "\u0022" + locationData.alternativeName1.toLowerCase() + "\u0022"
+            ) {
+                asnLocations.features.push(
+                    populateAsnData(
+                        locationData.abbreviation,
+                        locationData.name,
+                        locationData.coordinates
+                    )
+                );
+            } else if (
+                countryName.toLowerCase() ===
+                locationData.alternativeName2.toLowerCase() ||
+                countryName.toLowerCase() ===
+                "\u0022" + locationData.alternativeName2.toLowerCase() + "\u0022"
+            ) {
+                asnLocations.features.push(
+                    populateAsnData(
+                        locationData.abbreviation,
+                        locationData.name,
+                        locationData.coordinates
+                    )
+                );
+            } else if (
+                countryName.toLowerCase() ===
+                locationData.alternativeName3.toLowerCase() ||
+                countryName.toLowerCase() ===
+                "\u0022" + locationData.alternativeName3.toLowerCase() + "\u0022"
+            ) {
+                asnLocations.features.push(
+                    populateAsnData(
+                        locationData.abbreviation,
+                        locationData.name,
+                        locationData.coordinates
+                    )
+                );
+            } else if (
+                countryName.toLowerCase() ===
+                locationData.alternativeName4.toLowerCase() ||
+                countryName.toLowerCase() ===
+                "\u0022" + locationData.alternativeName4.toLowerCase() + "\u0022"
+            ) {
+                asnLocations.features.push(
+                    populateAsnData(
+                        locationData.abbreviation,
+                        locationData.name,
+                        locationData.coordinates
+                    )
+                );
+            } else if (
+                countryName.toLowerCase() ===
+                locationData.alternativeName5.toLowerCase() ||
+                countryName.toLowerCase() ===
+                "\u0022" + locationData.alternativeName5.toLowerCase() + "\u0022"
+            ) {
+                asnLocations.features.push(
+                    populateAsnData(
+                        locationData.abbreviation,
+                        locationData.name,
+                        locationData.coordinates
+                    )
+                );
+            } else if (
+                countryName.toLowerCase() ===
+                locationData.alternativeName6.toLowerCase() ||
+                countryName.toLowerCase() ===
+                "\u0022" + locationData.alternativeName6 + "\u0022"
+            ) {
+                asnLocations.features.push(
+                    populateAsnData(
+                        locationData.abbreviation,
+                        locationData.name,
+                        locationData.coordinates
+                    )
+                );
             }
         });
     });
 
-    return asnLocations;  //return the geojson object.
+    return asnLocations; //return the geojson object.
 }
-
 
 var exampleJson = geoJsonASNs();
 
 delete L.Icon.Default.prototype._getIconUrl;
 
 //apply blue pins as markers for the mapping.
+/*L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+  iconUrl: require("leaflet/dist/images/marker-icon.png"),
+  shadowUrl: require("leaflet/dist/images/marker-shadow.png")
+});*/
+//added due to error in codesandbox.io
 L.Icon.Default.mergeOptions({
-   iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-   iconUrl: require('leaflet/dist/images/marker-icon.png'),
-   shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+    iconRetinaUrl: require("./images/marker-icon-2x.png"),
+    iconUrl: require("./images/marker-icon.png"),
+    shadowUrl: require("./images/marker-shadow.png")
 });
 
 const initialAfricaZoom = 3; //set the initial zoom in so that the user sees the whole continent of Africa only.
-const coordinatesAfricaCenter = [7.943625, 24.329730]; //set center point so that continent appears centrally in the display.
+const coordinatesAfricaCenter = [7.943625, 24.32973]; //set center point so that continent appears centrally in the display.
 
 function App() {
     return (
         <div className="App">
-            <Map center={defaultCenter} zoom={defaultZoom}>
-                <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" attribution="&copy; <a href=&quot;https://www.openstreetmap.org/copyright&quot;>OpenStreetMap</a> contributors" />
+            <Map center={coordinatesAfricaCenter} zoom={initialAfricaZoom}>
+                <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                />
                 <GeoJSON data={exampleJson} />
             </Map>
         </div>
